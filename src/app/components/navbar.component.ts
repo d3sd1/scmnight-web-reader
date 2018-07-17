9da2c7ae59db6abc8d79ca042c00b5a4916b2564
Andrei Jause <andreigarciacuadra@gmail.com>
2018-07-18 01:15:25 +0200

    fixes mayores

--- src/app/components/navbar.component.ts
+++ src/app/components/navbar.component.ts
@@ -8,20 +8,98 @@ import {MenuRoutes} from '../kernel/config/routes.menu.config';
 import {MenuOptionData} from '../kernel/model/menu-option-data';
 import {SessionSingleton} from '../kernel/singletons/session.singleton';
 import {Permission} from "../kernel/model/Permission";
+import {MzModalComponent} from "ng2-materialize";
 
 @Component({
   selector: 'main-content',
   templateUrl: '../templates/navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router, private translate: TranslateService, private sessionInfo: SessionSingleton) {
+
+  options: UploaderOptions;
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
+
+  onUploadOutput(output: UploadOutput): void {
+    if (output.type === 'allAddedToQueue') { // when all files added in queue
+      // uncomment this if you want to auto upload files when added
+      // const event: UploadInput = {
+      //   type: 'uploadAll',
+      //   url: '/upload',
+      //   method: 'POST',
+      //   data: { foo: 'bar' }
+      // };
+      // this.uploadInput.emit(event);
+    } else if (output.type === 'addedToQueue'  && typeof output.file !== 'undefined') { // add file to array when added
+      this.files.push(output.file);
+    } else if (output.type === 'uploading' && typeof output.file !== 'undefined') {
+      // update current data in files array for uploading file
+      const index = this.files.findIndex(file => typeof output.file !== 'undefined' && file.id === output.file.id);
+      this.files[index] = output.file;
+    } else if (output.type === 'removed') {
+      // remove file from array when removed
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    } else if (output.type === 'dragOver') {
+      this.dragOver = true;
+    } else if (output.type === 'dragOut') {
+      this.dragOver = false;
+    } else if (output.type === 'drop') {
+      this.dragOver = false;
+    }
   }
+
+  startUpload(): void {
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: 'http://ngx-uploader.com/upload',
+      method: 'POST',
+      data: { foo: 'bar' }
+    };
+
+    this.uploadInput.emit(event);
+  }
+
+  cancelUpload(id: string): void {
+    this.uploadInput.emit({ type: 'cancel', id: id });
+  }
+
+  removeFile(id: string): void {
+    this.uploadInput.emit({ type: 'remove', id: id });
+  }
+
+  removeAllFiles(): void {
+    this.uploadInput.emit({ type: 'removeAll' });
+  }
+  /* end u`pload example */
+  constructor(private router: Router, private translate: TranslateService, private sessionInfo: SessionSingleton) {
+
+    //upload example
+    this.options = { concurrency: 1, maxUploads: 3 };
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+    this.humanizeBytes = humanizeBytes;
+  }
+  public modalOptions: Materialize.ModalOptions = {
+    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '100%', // Starting top style attribute
+    endingTop: '10%'
+  };
+  @ViewChild('changeLogoModal') changeLogoModal: MzModalComponent;
 
   filteredMenuOptions: MenuOption[] = [];
   menuLoaded: boolean = false;
   private user: User;
   private userPermission: Array<Permission>;
 
+  changeLogo() {
+    this.changeLogoModal.open();
+  }
   ngOnInit() {
     this.sessionInfo.getDiscoInfo().then(discoInfo => {
       this.sessionInfo.getUser().then(user => {
