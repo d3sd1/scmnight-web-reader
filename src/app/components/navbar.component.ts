f655c107fef3303c07229aaec624bbd280d8ba31
Andrei Jause <andreigarciacuadra@gmail.com>
2018-07-24 18:23:32 +0200

    chat users all working. all. everything.

--- src/app/components/navbar.component.ts
+++ src/app/components/navbar.component.ts
@@ -7,7 +7,7 @@ import {
   EventEmitter,
   Output,
   OnDestroy,
-  AfterViewInit
+  AfterViewInit, HostListener
 } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {User} from '../kernel/model/user';
@@ -29,6 +29,10 @@ import {ConflictReasonManage} from "../kernel/model/conflict-reason-manage";
 import {WsService} from "../kernel/services/ws.service";
 import {DomSanitizer, SafeStyle, SafeUrl} from "@angular/platform-browser";
 import {ToastrService} from "ngx-toastr";
+import {environment} from "../../environments/environment";
+import {HttpHeaders} from "@angular/common/http";
+import {ApiOptions} from "../kernel/config/api.config";
+import {SessionService} from "../kernel/services/session.service";
 
 @Component({
   selector: 'main-content',
@@ -45,29 +49,80 @@ export class NavbarComponent implements OnInit, OnDestroy {
   discoName: string = "";
   chatUsers: Array<User> = null;
   firstChatLoad = true;
+  inactivityMS: number = 5000;
+  userInactive: boolean = false;
+  inactivityTimeout = null;
+  settingInactive = false;
+  settingActive = false;
 
   @ViewChild('changeLogoModal') changeLogoModal: MzModalComponent;
 
-  loadChat() {
-    if (!this.ws.connected) {
-      this.chatUsers = null;
-      this.firstChatLoad = true;
-    }
-    else {
-      this.chatUsers = [];
-      /* Cargar usuarios del chat */
-      this.api.get("rest/chat/users").subscribe(
-        (users: Array<User>) => {
-          users.forEach((user: User, index: number) => {
-            this.onChatUpdate(() => {
-              if (index === users.length - 1) {
-                this.firstChatLoad = false;
-              }
-            }, user);
-          });
+  setInactive() {
+    if (!this.settingInactive) {
+      this.settingInactive = true;
+      this.api.post("rest/chat/user/inactive", {}).subscribe(
+        (user: User) => {
+          this.user.chat_status.chat_status = "IDLE";
+          this.userInactive = true;
+          this.settingInactive = false;
+          this.onChatUpdate(null, user);
         }, () => {
-          this.firstChatLoad = false;
+          this.user.chat_status.chat_status = "IDLE";
+          this.userInactive = true;
+          this.settingInactive = false;
         });
+    }
+  }
+
+  setActive() {
+    if (!this.settingActive) {
+      this.settingActive = true;
+      this.api.post("rest/chat/user/active", {}).subscribe(
+        (user: User) => {
+          this.user.chat_status.chat_status = "ONLINE";
+          this.userInactive = false;
+          this.settingActive = false;
+          this.onChatUpdate(null, user);
+        }, () => {
+          this.user.chat_status.chat_status = "ONLINE";
+          this.userInactive = false;
+          this.settingActive = false;
+        });
+    }
+  }
+
+  refreshInactivity($event) {
+    if (this.canUseChat) {
+      if (this.userInactive) {
+        this.setActive();
+      }
+      clearTimeout(this.inactivityTimeout);
+      this.inactivityTimeout = setTimeout(() => this.setInactive(), this.inactivityMS);
+    }
+  }
+
+  loadChat() {
+    if (this.canUseChat) {
+      if (!this.ws.connected) {
+        this.chatUsers = null;
+        this.firstChatLoad = true;
+      }
+      else {
+        this.chatUsers = [];
+        /* Cargar usuarios del chat */
+        this.api.get("rest/chat/users").subscribe(
+          (users: Array<User>) => {
+            users.forEach((user: User, index: number) => {
+              this.onChatUpdate(() => {
+                if (index === users.length - 1) {
+                  this.firstChatLoad = false;
+                }
+              }, user);
+            });
+          }, () => {
+            this.firstChatLoad = false;
+          });
+      }
     }
   }
 
@@ -139,7 +194,8 @@ export class NavbarComponent implements OnInit, OnDestroy {
     };
   }
 
-  constructor(public router: Router, private _sanitizer: DomSanitizer, public ws: WsService, private loadingBar: LoadingBarService, private api: ApiService, private translate: TranslateService, private sessionInfo: SessionSingleton, private toastr: ToastrService) {
+  constructor(public router: Router, private _sanitizer: DomSanitizer, public ws: WsService, private loadingBar: LoadingBarService, private api: ApiService, private translate: TranslateService, private sessionInfo: SessionSingleton, private toastr: ToastrService,
+  private sessMan:SessionService) {
 
     this.sessionInfo.getPermissions().then(res => {
       this.canEditLogo = res.findIndex(x => x.action === "CHANGE_LOGO") !== -1;
@@ -182,9 +238,18 @@ export class NavbarComponent implements OnInit, OnDestroy {
     if (user.dni == this.user.dni) {
       return false;
     }
+    else if (user.dni == this.user.dni && !this.firstChatLoad) {
+      this.user.chat_status.chat_status = user.chat_status.chat_status;
+      return false;
+    }
 
     /* check if user exists */
     const actualUserIndex = this.chatUsers.findIndex(x => x.dni == user.dni);
+    let reconnectedFromAfk: boolean = false;
+    if (actualUserIndex !== -1 && this.chatUsers[actualUserIndex].chat_status.chat_status == "IDLE") {
+      reconnectedFromAfk = true;
+    }
+
     if (actualUserIndex !== -1) {
       this.chatUsers[actualUserIndex] = user;
     }
@@ -194,7 +259,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
     /* alert actual user if needed */
     this.sessionInfo.getUser().then(sessUser => {
-      if (!this.firstChatLoad && sessUser.chat_notifications) {
+      if (!this.firstChatLoad && sessUser.chat_notifications && !reconnectedFromAfk) {
         if (user.chat_status.chat_status == "ONLINE") {
           this.toastr.info(user.firstname + " " + user.lastname + " se ha conectado al chat.", "", {
             timeOut: 2000,
@@ -239,7 +304,21 @@ export class NavbarComponent implements OnInit, OnDestroy {
     });
   }
 
+
+  /* Set user status when closing browser */
+  @HostListener('window:beforeunload', ['$event'])
+  public beforeunloadHandler($event) {
+    console.log("Ending chat..");
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST",environment.baseUrl + "rest/chat/user/offline",false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader(ApiOptions.headerName, ApiOptions.authScheme + this.sessMan.getToken());
+    xhr.send();
+  }
+
   ngOnInit() {
+    this.refreshInactivity(null);
+    this.setActive();
     this.ws.subscribe("scm/config_logo", this.onLogoChanged.bind(this));
     this.ws.subscribe("scm/chat_users", this.onChatUpdate.bind(this));
 
